c6ec641db059dc297802d293dae0633d05d0ba5b
syf_ <shuyifei2001@gmail.com>
2024-09-17 01:00:53 +0800

    002

---
+++ tamp_001.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         temp_test_yandong
+// @namespace    http://tampermonkey.net/
+// @version      2024-09-16
+// @description  try to take over the world!
+// @author       You
+// @match        https://blog.csdn.net/*/article/details/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=greasyfork.org
+// @grant        none
+// @license      AGPL-3.0
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    console.log('Test!!!!')
+    // Your code here...
+})();
